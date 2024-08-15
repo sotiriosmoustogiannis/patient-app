b57390ee09c31e19edc9297b084d2734fdb16a54
sotiriosmoustogiannis <sotomous@outlook.com>
2024-08-15 14:05:19 +0300

    Update validations onUtils

--- client/src/utils/validationHelpers.ts
+++ client/src/utils/validationHelpers.ts
@@ -1,4 +1,4 @@
-import { validateRequiredField, validateOnlyLetters, validatePhoneNumber, validateBirthDate } from './validations';
+import { validateRequiredField, validateOnlyLettersSpaces, validatePhoneNumber, validateBirthDate } from './validations';
 import { formatBirthDate, formatPhoneNumber } from './formats';
 import { FieldName } from '../types/types';
 
@@ -12,22 +12,41 @@ export const validateAndFormatField = (name: FieldName, value: string): Validati
   let formattedValue = value;
 
   if (!validateRequiredField(value)) {
-    validationError = 'This field is required';
-  } else if (name === 'name' || name === 'surname') {
-    if (!validateOnlyLetters(value)) {
-      validationError = 'Only letters and spaces are allowed';
-    }
-  } else if (name === 'phone') {
-    if (!validatePhoneNumber(value)) {
-      validationError = 'Invalid phone number';
-    } else {
-      formattedValue = formatPhoneNumber(value);
-    }
-  } else if (name === 'birthDate') {
-    formattedValue = formatBirthDate(value);
-    if (!validateBirthDate(formattedValue)) {
-      validationError = 'Invalid date format';
-    }
+    return {
+      error: 'This field is required',
+      formattedValue: value
+    };
+  }
+
+  // Validate and format based on field type
+  switch (name) {
+    case 'name':
+    case 'surname':
+      if (!validateOnlyLettersSpaces(value)) {
+        validationError = 'Only Latin letters and spaces are allowed';
+      }
+      break;
+
+    case 'phone':
+      if (!validatePhoneNumber(value)) {
+        validationError = 'Invalid phone number';
+      } else {
+        formattedValue = formatPhoneNumber(value);
+      }
+      break;
+
+    case 'birthDate':
+      formattedValue = formatBirthDate(value);
+      if (!validateBirthDate(formattedValue)) {
+        validationError = 'Invalid date format';
+      }
+      break;
+
+    case 'text':
+      break;
+
+    default:
+      validationError = 'Invalid field type';
   }
 
   return {
--- client/src/utils/validations.ts
+++ client/src/utils/validations.ts
@@ -1,54 +1,65 @@
+// Check if a field is empty
 export const validateRequiredField = (value: string): boolean => {
   return value.trim() !== "";
 };
 
-export const validateOnlyLetters = (input: string): boolean => {
-  const lettersOnlyRegex = /^[A-Za-z\s]+$/;
-  return lettersOnlyRegex.test(input.trim());
+// Check if a field contains only latin letters and spaces
+export const validateOnlyLettersSpaces = (value: string): boolean => {
+  const lettersSpacesOnlyRegex = /^[A-Za-z\s]+$/;
+  return lettersSpacesOnlyRegex.test(value.trim());
 };
 
-
 export const validatePhoneNumber = (phoneNumber: string): boolean => {
-  const greekPhoneRegex = /^(?:\+30\s?|30\s?)?\d{10}$/
+  const greekPhoneRegex = /^(?:\+30\s?)?\d{10}$/
   return greekPhoneRegex.test(phoneNumber);
 };
 
+// Function to validate the birth date
 export const validateBirthDate = (date: string): boolean => {
   if (date.length !== 10) {
     return false;
   }
-  console.log('validate')
 
-  // YYYY-MM-DD format
+  // Check the format YYYY-MM-DD
   const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
-
   if (!dateRegex.test(date)) {
     return false;
   }
 
+  // Get birth year, month, and day splitting by '-'
   const [year, month, day] = date.split('-').map(Number);
-  const currentDate = new Date();
-  const currentYear = currentDate.getFullYear();
 
-  // Validate year
-  if (year > currentYear) {
+  // Validate the number of days based on month and leap year
+  const daysInMonth = [31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (month < 1 || month > 12 || day < 1 || day > daysInMonth[month - 1]) {
     return false;
   }
 
-  // Validate month
-  if (month < 1 || month > 12) {
-    return false;
-  }
-
-  // Validate number of day based on month and leap year
-  const daysInMonth = [31, (isLeapYear(year) ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  if (day < 1 || day > daysInMonth[month - 1]) {
-    return false;
-  }
-
-  return true;
+  // Use the function to check if the entered date is not in the future
+  return !isFutureDate(year, month, day);
 };
 
+// Check if the entered date is in the future
+const isFutureDate = (year: number, month: number, day: number): boolean => {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth() + 1;
+  const currentDay = currentDate.getDate();
+
+  if (year > currentYear) {
+    return true;
+  }
+  if (year === currentYear && month > currentMonth) {
+    return true;
+  }
+  if (year === currentYear && month === currentMonth && day > currentDay) {
+    return true;
+  }
+
+  return false;
+};
+
+// Function to check if a given year is a leap year
 const isLeapYear = (year: number): boolean => {
   return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 };
